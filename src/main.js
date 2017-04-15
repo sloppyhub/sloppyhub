9f30da77dc7d943dd7905439ef7e209acaac2f0b
mofon <immofon@163.com>
2017-04-15 16:55:28 +0800

    mv Main to App

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Main from './Main'
+import App from './App'
 import router from './router'
 import store from './store/'
 import VueMaterial from 'vue-material'
@@ -14,8 +14,8 @@ new Vue({
     el: '#app',
     router,
     store,
-    template: '<Main/>',
+    template: '<App />',
     components: {
-        Main,
+        App,
     }
 })
