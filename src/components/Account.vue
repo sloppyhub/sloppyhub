e8a8b155d85b697c7c3ed68209c30613e25fdff5
mofon <immofon@163.com>
2017-04-15 19:01:26 +0800

    add components/Account

---
+++ src/components/Account.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <md-card>
+            <md-card-header>
+                <span class="md-title">{{ userId }}</span>
+                <md-button class="md-primary" @click.native="$refs['dlg-token'].open()">get token</md-button>
+            </md-card-header>
+            <md-card-content>
+                <project v-for="project in projects" :key="project.project" :project="project.project" :services="project.services" />
+            </md-card-content>
+        </md-card>
+        <md-dialog ref="dlg-token">
+            <div style="width: 800px;"></div>
+            <md-dialog-title>Access Token</md-dialog-title>
+            <md-dialog-content>
+                <div style="word-wrap: break-word;">{{ token }}</div>
+            </md-dialog-content>
+        </md-dialog>
+    </div>
+</template>
+<script>
+import Project from '@/components/Project'
+
+export default {
+    name: 'account',
+    props: {
+        userId: {
+            type: String,
+        },
+        token: {
+            type: String,
+        },
+        projects: {
+            type: Array,
+        },
+    },
+    components: {
+        Project,
+    },
+}
+</script>
--- src/view/Dev.vue
+++ src/view/Dev.vue
@@ -4,17 +4,21 @@
         <service :id="service.id" :apps="service.apps" />
         <h2>components/Project</h2>
         <project :project="project.project" :services="project.services" />
+        <h2>components/Account</h2>
+        <account :userId="account.userId" :token="account.token" :projects="account.projects" />
     </div>
 </template>
 <script>
 import Service from '@/components/Service'
 import Project from '@/components/Project'
+import Account from '@/components/Account'
 
 export default {
     name: 'dev',
     components: {
         Service,
         Project,
+        Account,
     },
     data() {
         return {
@@ -112,6 +116,79 @@ export default {
                     }]
                 }],
                 "project": "exposer"
+            },
+            account: {
+                'userId': 'jlihsef',
+                'token': 'ljf3aijfln8u525mp5faebawn6wn6n6nabyv4ybvayuenynaetyvg3VRWTWBTWBBBWTWQFAAJLIWy2cya==',
+                'projects': [{
+                    "services": [{
+                        "id": "frontend",
+                        "apps": [{
+                            "domain": {
+                                "uri": "exposer.sloppy.zone"
+                            },
+                            "mem": 256,
+                            "image": "mofon/exposer",
+                            "instances": 1,
+                            "lastFailure": null,
+                            "port_mappings": [{
+                                "container_port": 9000
+                            }],
+                            "status": [
+                                "running"
+                            ],
+                            "id": "exposer-daemon",
+                            "versions": [
+                                "2017-03-29T06:27:17.832Z"
+                            ],
+                            "version": "2017-03-29T06:27:17.832Z",
+                            "env": {
+                                "FORWARD_KEY": "jvw3mfrrnwvlrjif"
+                            }
+                        }]
+                    }, {
+                        "id": "backend",
+                        "apps": [{
+                            "lastFailure": null,
+                            "mem": 256,
+                            "image": "vimagick/shadowsocks",
+                            "instances": 1,
+                            "env": {
+                                "PASSWORD": "afvbwtnmy3"
+                            },
+                            "versions": [
+                                "2017-03-29T06:27:17.832Z"
+                            ],
+                            "version": "2017-03-29T06:27:17.832Z",
+                            "id": "ss",
+                            "status": [
+                                "running"
+                            ]
+                        }, {
+                            "lastFailure": null,
+                            "instances": 1,
+                            "image": "mofon/docker-gotty",
+                            "mem": 256,
+                            "volumes": [{
+                                "size": "8GB",
+                                "container_path": "/data"
+                            }],
+                            "versions": [
+                                "2017-03-29T06:27:17.832Z"
+                            ],
+                            "version": "2017-03-29T06:27:17.832Z",
+                            "env": {
+                                "USERNAME": "mofon",
+                                "PASSWORD": "fsaby5v3vtat"
+                            },
+                            "id": "gotty",
+                            "status": [
+                                "running"
+                            ]
+                        }]
+                    }],
+                    "project": "exposer"
+                }],
             },
         }
 
