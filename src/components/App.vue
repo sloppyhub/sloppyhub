0194d6a55c7bb128e6b2ffa3036ec4e11c68465b
mofon <immofon@163.com>
2017-04-15 16:56:16 +0800

    add view/Dev components/App

---
+++ src/components/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="app">
+        <md-card>
+            <md-card-header>
+                <div class="md-title">{{ id }}</div>
+                <div class="md-subtitle">{{ image }}</div>
+            </md-card-header>
+            <md-card-content>
+                <md-list>
+                    <md-list-item>mem: {{ mem }} MB</md-list-item>
+                    <md-list-item>instances: {{ instances }}</md-list-item>
+                    <md-list-item>
+                        <span>env</span>
+                        <md-list-expand>
+                            <md-list>
+                                <md-list-item v-for="($value,$key) in env" :key="$key">
+                                    {{ $key }} : {{ $value }}
+                                </md-list-item>
+                            </md-list>
+                        </md-list-expand>
+                    </md-list-item>
+                </md-list>
+            </md-card-content>
+        </md-card>
+    </div>
+</template>
+<script>
+export default {
+    name: 'app',
+    props: {
+        id: {
+            type: String
+        },
+        image: {
+            type: String
+        },
+        instances: {
+            type: Number
+        },
+        mem: {
+            type: Number
+        },
+        env: {
+            type: Object
+        },
+    },
+}
+</script>
+<style scoped>
+</style>
---
+++ src/view/Dev.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <App id="hello world" image="mofon/hello" :env="env" :instances="1" :mem="256" />
+    </div>
+</template>
+<script>
+import App from '@/components/App'
+
+export default {
+    name: 'dev',
+    components: {
+        App,
+    },
+    data() {
+        return {
+            env: {
+                hello: "world",
+            }
+        }
+    },
+}
+</script>
