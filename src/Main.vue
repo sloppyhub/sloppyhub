976a2f6318a65b64d2e6ceffd01ee531b99efc3e
mofon <immofon@163.com>
2017-04-15 15:56:42 +0800

    mv App.vue to Main.vue

--- src/Main.vue
+++ src/Main.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
+  <div id="main">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'main'
 }
 </script>
 
