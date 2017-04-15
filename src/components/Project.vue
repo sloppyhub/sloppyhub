5dfb14d936bd6e8549de8e71e7ec32bc53644336
mofon <immofon@163.com>
2017-04-15 17:39:59 +0800

    add components/Project

---
+++ src/components/Project.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <md-card>
+            <md-card-header>
+                <div class="md-title">{{ project }}</div>
+            </md-card-header>
+            <md-card-content>
+                <service v-for="service in services" :key="service.id" :id="service.id" :apps="service.apps" />
+            </md-card-content>
+        </md-card>
+    </div>
+</template>
+<script>
+import Service from '@/components/Service'
+
+export default {
+    name: 'project',
+    props: {
+        project: {
+            type: String,
+        },
+        services: {
+            type: Array,
+        },
+    },
+    components: {
+        Service,
+    }
+}
+</script>
--- src/view/Dev.vue
+++ src/view/Dev.vue
@@ -1,44 +1,127 @@
 <template>
-    <div>
-        <service :id="id" :apps="apps" />
+    <div id="dev">
+        <h2>components/Service</h2>
+        <service :id="service.id" :apps="service.apps" />
+        <h2>components/Project</h2>
+        <project :project="project.project" :services="project.services" />
     </div>
 </template>
 <script>
 import Service from '@/components/Service'
+import Project from '@/components/Project'
 
 export default {
     name: 'dev',
     components: {
         Service,
+        Project,
     },
     data() {
         return {
-            "id": "frontend",
-            "apps": [{
-                "domain": {
-                    "uri": "exposer.sloppy.zone"
-                },
-                "mem": 256,
-                "image": "mofon/exposer",
-                "instances": 1,
-                "lastFailure": null,
-                "port_mappings": [{
-                    "container_port": 9000
+            service: {
+                "id": "frontend",
+                "apps": [{
+                    "domain": {
+                        "uri": "exposer.sloppy.zone"
+                    },
+                    "mem": 256,
+                    "image": "mofon/exposer",
+                    "instances": 1,
+                    "lastFailure": null,
+                    "port_mappings": [{
+                        "container_port": 9000
+                    }],
+                    "status": [
+                        "running"
+                    ],
+                    "id": "exposer-daemon",
+                    "versions": [
+                        "2017-03-29T06:27:17.832Z"
+                    ],
+                    "version": "2017-03-29T06:27:17.832Z",
+                    "env": {
+                        "FORWARD_KEY": "jvw3mfrrnwvlrjif"
+                    }
+                }]
+            },
+            project: {
+                "services": [{
+                    "id": "frontend",
+                    "apps": [{
+                        "domain": {
+                            "uri": "exposer.sloppy.zone"
+                        },
+                        "mem": 256,
+                        "image": "mofon/exposer",
+                        "instances": 1,
+                        "lastFailure": null,
+                        "port_mappings": [{
+                            "container_port": 9000
+                        }],
+                        "status": [
+                            "running"
+                        ],
+                        "id": "exposer-daemon",
+                        "versions": [
+                            "2017-03-29T06:27:17.832Z"
+                        ],
+                        "version": "2017-03-29T06:27:17.832Z",
+                        "env": {
+                            "FORWARD_KEY": "jvw3mfrrnwvlrjif"
+                        }
+                    }]
+                }, {
+                    "id": "backend",
+                    "apps": [{
+                        "lastFailure": null,
+                        "mem": 256,
+                        "image": "vimagick/shadowsocks",
+                        "instances": 1,
+                        "env": {
+                            "PASSWORD": "afvbwtnmy3"
+                        },
+                        "versions": [
+                            "2017-03-29T06:27:17.832Z"
+                        ],
+                        "version": "2017-03-29T06:27:17.832Z",
+                        "id": "ss",
+                        "status": [
+                            "running"
+                        ]
+                    }, {
+                        "lastFailure": null,
+                        "instances": 1,
+                        "image": "mofon/docker-gotty",
+                        "mem": 256,
+                        "volumes": [{
+                            "size": "8GB",
+                            "container_path": "/data"
+                        }],
+                        "versions": [
+                            "2017-03-29T06:27:17.832Z"
+                        ],
+                        "version": "2017-03-29T06:27:17.832Z",
+                        "env": {
+                            "USERNAME": "mofon",
+                            "PASSWORD": "fsaby5v3vtat"
+                        },
+                        "id": "gotty",
+                        "status": [
+                            "running"
+                        ]
+                    }]
                 }],
-                "status": [
-                    "running"
-                ],
-                "id": "exposer-daemon",
-                "versions": [
-                    "2017-03-29T06:27:17.832Z"
-                ],
-                "version": "2017-03-29T06:27:17.832Z",
-                "env": {
-                    "FORWARD_KEY": "jvw3mfrrnwvlrjif"
-                }
-            }]
+                "project": "exposer"
+            },
         }
 
     },
 }
 </script>
+<style scoped>
+#dev {
+    padding: 0 20px;
+    max-width: 860px;
+    margin: 20px auto 0;
+}
+</style>
