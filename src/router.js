4b8c3a07f53c05cb97895044809c1533b979ff32
mofon <immofon@163.com>
2017-04-15 15:49:30 +0800

    rm assets/logo.png and components/Hello

--- src/router.js
+++ src/router.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+    routes: [{
+        path: '/',
+        name: 'dev',
+        component: {
+            template: '<h1>Development</h1>',
+        }
+    }]
 })
