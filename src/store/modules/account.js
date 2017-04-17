98607441a5afaa538ba8140c632cb8e7b9c7c4af
mofon <immofon@163.com>
2017-04-18 07:45:29 +0800

    add getters map() on store/account

--- src/store/modules/account.js
+++ src/store/modules/account.js
@@ -6,42 +6,65 @@ const type = {
     SET_ALL: 'SET_ALL',
 }
 
+const helper = {
+    exist(accounts, { key, value }) {
+        if (key == undefined || key == '' || value == undefined) {
+            return false
+        }
+
+        let found = accounts.find((account) => {
+            return account[key] === value
+        })
+
+        return found !== undefined
+    },
+    existUserId(accounts, { userId }) {
+        return this.exist(accounts, {
+            key: 'userId',
+            value: userId,
+        })
+    },
+}
+
 export default {
     namespaced: true,
     state: {
         accounts: [], // { userId,token } 
     },
+    getters: {
+        map(state) { // userId as key
+            let m = {}
+            state.accounts.forEach(account => {
+                m[account.userId] = account
+            })
+            return m
+        },
+    },
     actions: {
-        exist({ state }, { userId }) {
-            let foundAccount = state.accounts.find(account => {
-                return account.userId === userId
-            })
-            return foundAccount !== undefined
-        },
-        getByUserId({ state }, { userId }) {
-            return state.accounts.find(account => {
-                return account.userId === userId
-            })
-        },
-        add({ commit, dispatch }, { userId, token }) {
+        add({ state, commit, dispatch }, { userId, token }) {
             return new Promise((resolve, reject) => {
-                dispatch('exist', { userId }).then(exist => {
-                    if (exist) {
-                        throw new Error('userId exist')
-                    }
+                if (helper.existUserId(state.accounts, { userId })) {
+                    reject(Error(`userId ${userId} is exist`))
+                }
 
-                    commit(type.ADD, { userId, token })
-                    dispatch('save').then(() => {
-                        resolve()
-                    })
-                }).catch((err) => {
+                commit(type.ADD, { userId, token })
+                dispatch('save').then(() => {
+                    resolve()
+                }).catch(err => {
                     reject(err)
                 })
+
             })
         },
         remove({ commit, dispatch }, { userId }) {
-            commit(type.REMOVE, { userId })
-            dispatch('save')
+            return new Promise((resolve, reject) => {
+                commit(type.REMOVE, { userId })
+                dispatch('save').then(() => {
+                    resolve()
+                }).catch(err => {
+                    reject(err)
+                })
+            })
         },
         load({ commit }) {
             commit(type.SET_ALL, { accounts: storage.loadAccounts() })
@@ -52,10 +75,7 @@ export default {
     },
     mutations: {
         [type.ADD](state, { userId, token }) {
-            let foundAccount = state.accounts.find(account => {
-                return account.userId === userId
-            })
-            if (foundAccount !== undefined) {
+            if (userId == undefined || token == undefined || helper.existUserId(state.accounts, { userId })) {
                 return
             }
             state.accounts.unshift({
